4f0d5353908e196914df610ec72e5d9fd3f69b43
kk <kentokosone@kentokosonenoMacBook-Pro.local>
2023-07-13 19:45:22 +0900

    SSG生成

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -18,7 +18,7 @@ async function fetchBlog(blogId: string) {
       headers: new Headers({
         apikey: process.env.apikey as string,
       }),
-      cache: 'no-store',
+      cache: 'force-cache',
     }
   )
   //   if (!res.ok) {
@@ -45,4 +45,17 @@ export default async function BlogDetailPage({ params }: PageProps) {
       </Link>
     </div>
   )
+}
+
+export async function generateStaticParams() {
+  const res = await fetch(`${process.env.url}/rest/v1/blogs?select=*`, {
+    headers: new Headers({
+      apikey: process.env.apikey as string,
+    }),
+    cache: 'force-cache',
+  })
+  const blogs: Blog[] = await res.json()
+  return blogs.map((b) => ({
+    blogId: b.id.toString(),
+  }))
 }
--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <header>
       <nav>
-        <Link href="/">Home</Link>
+        {/* <Link href="/">Home</Link> */}
         <Link href="/blogs">blogs</Link>
         {/* <Link href='/streaming-sr'>
                     Streaming SR
