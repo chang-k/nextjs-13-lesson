625714b37d5895afe6f7ef89ba8c1ec8f1dc28d6
kk <necessary.sone@gmail.com>
2023-09-09 20:51:21 +0900

    日本語変換のエンターでは発火しないようにする&編集モードにした時にフォーカス

--- src/app/components/td.tsx
+++ src/app/components/td.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { TableCell, type TableForm } from '../table/FormProvider/useTableForm'
 
@@ -11,23 +11,42 @@ type Props = {
 
 export default function Td({ accesorName, fieldRowCol }: Props) {
   const { register } = useFormContext<TableForm>()
+  console.log('register(accesorName)', register(accesorName).ref)
+
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const [mode, setMode] = useState<'button' | 'text' | 'edit'>(
     fieldRowCol === null ? 'button' : 'text'
   )
-
   const [colTitle, setColTitle] = useState(fieldRowCol?.title ?? '')
+  const [composing, setComposition] = useState(false)
+
+  const startComposition = useCallback(() => setComposition(true), [])
+  const endComposition = useCallback(() => setComposition(false), [])
 
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
-      if (event.key === 'Enter' && mode === 'edit') {
+      if (event.key === 'Enter' && !composing) {
         event.preventDefault()
-        setMode('text')
+        if (colTitle !== '') {
+          setMode('text')
+        } else {
+          setMode('button')
+        }
       }
     }
     window.addEventListener('keydown', handleKeyDown)
+    console.log('useEffect')
     return () => window.removeEventListener('keydown', handleKeyDown)
-  }, [mode])
+  }, [mode, colTitle, composing])
+
+  useEffect(() => {
+    if (!!inputRef.current && mode === 'edit') {
+      inputRef.current.focus()
+      console.log('useEffect2')
+    }
+    console.log('useEffect3')
+  }, [inputRef, mode])
 
   if (mode === 'button') {
     return (
@@ -42,7 +61,9 @@ export default function Td({ accesorName, fieldRowCol }: Props) {
     return (
       <td>
         {colTitle}
-        <button onClick={() => setMode('edit')}>編集</button>
+        <button onClick={() => setMode('edit')} type="button">
+          編集
+        </button>
       </td>
     )
   }
@@ -50,6 +71,8 @@ export default function Td({ accesorName, fieldRowCol }: Props) {
     <td>
       <input
         {...register(accesorName)}
+        ref={inputRef}
+        value={colTitle}
         onChange={(e) => {
           register(accesorName).onChange(e)
           setColTitle(e.target.value)
@@ -57,12 +80,22 @@ export default function Td({ accesorName, fieldRowCol }: Props) {
         onBlur={(e) => {
           register(accesorName).onBlur(e)
           setMode('text')
+          console.log('e.target.value onBlur', e.target.value)
+          if (colTitle !== '') {
+            setColTitle(e.target.value)
+            setMode('text')
+          } else {
+            setMode('button')
+          }
         }}
+        // useEffectとどっちがいいか迷い中。今はぶっちゃけ雰囲気でやりたいだけ
         // onKeyDown={(event) => {
         //   if (event.key === 'Enter') {
         //     event.preventDefault()
         //   }
         // }}
+        onCompositionStart={startComposition}
+        onCompositionEnd={endComposition}
       />
     </td>
   )
